3fc72ca939828275ebb2f231ed40809a2f995e28
lxk <aqlynn@gmail.com>
2017-01-26 00:39:07 +0800

    p

--- bledemo/FunctionViewController.swift
+++ bledemo/FunctionViewController.swift
@@ -10,9 +10,21 @@ import UIKit
 
 class FunctionViewController: UIViewController {
     
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var labelFunction: UILabel!
     var type:Int!
         override func viewDidLoad() {
         super.viewDidLoad()
+            if(type==1){
+                 labelFunction.text="防丢系统"
+                 image.image=UIImage(named: "1")
+            }else if(type==2){
+                labelFunction.text="防盗系统"
+                image.image=UIImage(named: "3")
+            }else if(type==0){
+                labelFunction.text="儿童安全"
+                image.image=UIImage(named: "2")
+            }
 
 
         // Do any additional setup after loading the view.
